32ba4352b51e61ad9144d4c9b1d4667283b884a8
phouse512 <philiphouse2015@u.northwestern.edu>
2015-09-30 23:25:12 -0500

    mm good

--- src/main/java/com/example/helloworld/health/TemplateHealthCheck.java
+++ src/main/java/com/example/helloworld/health/TemplateHealthCheck.java
@@ -13,7 +13,7 @@ public class TemplateHealthCheck extends HealthCheck {
 
     @Override
     protected Result check() throws Exception {
-        template.render(Optional.of("woo"));
+        template.render(Optional.of("Testing the health check!"));
         template.render(Optional.<String>absent());
         return Result.healthy();
     }
